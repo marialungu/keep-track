2ff289efd7c9d269510871b79ec6f35f77c71daf
marialungu <anauna.lungu@gmail.com>
2022-03-30 21:37:26 +0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./tailwind.generated.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo text-yellow-400" alt="logo" />
+        <p className="text-yellow-400">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-yellow-400"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
